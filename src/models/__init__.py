b1e00d4a2a322725612fe0e1c7f7d79d5ca1c70c
AboALhasanx <hasanig227@gmail.com>
2026-04-22 20:22:49 +0300

    Refactor import statements for clarity

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
-from .blocks import Block, InlineNode  # noqa: F401
+from .blocks import Block  # noqa: F401
+from .inlines import InlineNode  # noqa: F401
 from .meta import Meta  # noqa: F401
 from .project import Project  # noqa: F401
 from .subject import Subject  # noqa: F401
 from .theme import Theme  # noqa: F401
-
